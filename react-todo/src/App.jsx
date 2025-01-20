195fb75969d058f1353e288ed629a628b135a517
meganw22 <mwalford22@gmail.com>
2025-01-20 18:48:00 +0000

    Added ListHeader.jsx and rendered Header to Vite

--- react-todo/src/App.jsx
+++ react-todo/src/App.jsx
@@ -1,8 +1,18 @@
 import { useState } from "react";
-import ToDoList from "./components/ToDoList";
+// import ToDoList from "./components/ToDoList";
+import ListHeader from "./components/ListHeader";
+import "./components/styles.css";
 
 const App = () => {
-  return <div className="container"></div>;
+  return (
+    <>
+      <div className="container">
+        <div className="app-wrapper">
+          <ListHeader />
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default App;
---
+++ react-todo/src/components/ListHeader.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ListHeader = () => {
+  return (
+    <div className="header">
+      <h1>To Do List</h1>
+    </div>
+  );
+};
+
+export default ListHeader;
