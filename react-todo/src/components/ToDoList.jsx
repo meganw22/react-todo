c19f126160d60a8b323e9fd8ebb7f5602741ebac
meganw22 <mwalford22@gmail.com>
2025-01-20 17:58:42 +0000

    Removed non utilised files and added fontawesome to head element

---
+++ react-todo/src/components/ToDoList.jsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+import "./styles.css";
+
+function App() {
+  const [tasks, setTasks] = useState([]);
+  const [newTask, setNewTask] = useState("");
+
+  const addTask = () => {
+    setNewTask;
+  };
+}
