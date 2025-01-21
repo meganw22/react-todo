e559d358a99aa5fb6a4397e17d712cab60584d86
meganw22 <mwalford22@gmail.com>
2025-01-21 21:04:54 +0000

    Created newTask and allTasks variables and gave them a function within Form.jsx

--- react-todo/src/App.jsx
+++ react-todo/src/App.jsx
@@ -5,13 +5,20 @@ import Form from "./components/Form";
 import "./components/styles.css";
 
 const App = () => {
+  const [newTask, setNewTask] = useState("");
+  const [allTasks, setAllTasks] = useState([]);
   return (
     <>
       <div className="container">
         <div className="app-wrapper">
           <ListHeader />
           <div>
-            <Form />
+            <Form
+              newTask={newTask}
+              setNewTask={setNewTask}
+              allTasks={allTasks}
+              setAllTasks={setAllTasks}
+            />
           </div>
         </div>
       </div>
--- react-todo/src/components/Form.jsx
+++ react-todo/src/components/Form.jsx
@@ -1,10 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 
-const Form = () => {
+const Form = ({ allTasks, setAllTasks, newTask, setNewTask }) => {
+  const onNewTaskChange = (event) => {
+    setNewTask(event.target.value);
+  };
+  const onFormSubmit = (event) => {
+    event.preventDefault();
+    if (newTask.trim() === "") return;
+
+    setAllTasks([...allTasks, newTask]);
+    setNewTask("");
+  };
+
   return (
     <div>
-      <form>
-        <input type="text" placeholder="Hello" className="task-input" />
+      <form onSubmit={onFormSubmit}>
+        <input
+          type="text"
+          placeholder="Hello"
+          className="task-input"
+          value={newTask}
+          required
+          onChange={onNewTaskChange}
+        />
         <button className="button-add" type="submit">
           Add
         </button>
