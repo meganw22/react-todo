193399194f9337f062daaaebb0f26008dcbf3f8d
meganw22 <mwalford22@gmail.com>
2025-01-23 18:08:34 +0000

    Added handleDelete fucntion  to delete a task from the list

--- react-todo/src/components/ToDoList.jsx
+++ react-todo/src/components/ToDoList.jsx
@@ -1,19 +1,36 @@
 import React from "react";
 
 const ToDoList = ({ allTasks, setAllTasks }) => {
+  const handleDelete = ({ id }) => {
+    setAllTasks(allTasks.filter((task) => task.id !== id));
+  };
   return (
-    <div>
+    <ul className="task-list">
       {allTasks.map((task) => (
-        <li className="todo-list" key={task.id}>
+        <li className="list-item" key={task.id}>
           <input
             type="text"
             value={task.title}
             className="list"
             onChange={(event) => event.preventDefault()}
           />
+          <div>
+            <button className="button-complete task-button">
+              <i className="far fa-edit"></i>
+            </button>
+            <button className="button-edit task-button">
+              <i className="far fa-check-square"></i>
+            </button>
+            <button
+              className="button-delete task-button"
+              onClick={() => handleDelete(task)}
+            >
+              <i className="fas fa-trash"></i>
+            </button>
+          </div>
         </li>
       ))}
-    </div>
+    </ul>
   );
 };
 
